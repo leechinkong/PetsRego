ab77ec60bc48c58df2d07f87ba1628438a2c5027
lee.c.kong@gmail.com <lee.kong@anz.com>
2018-08-06 12:14:09 +1000

    add sa key to deployment

--- src/main/kotlin/com/example/petsrego/model/Pet.kt
+++ src/main/kotlin/com/example/petsrego/model/Pet.kt
@@ -3,16 +3,16 @@ package com.example.petsrego.model
 import com.fasterxml.jackson.annotation.JsonIgnore
 import org.springframework.cloud.gcp.data.spanner.core.mapping.PrimaryKey
 import org.springframework.cloud.gcp.data.spanner.core.mapping.Table
-import java.util.UUID
+import java.util.*
 
 @Table(name = "pet")
-data class Pet(val name: String,
-               val type: String = "n/a",
-               val breed: String = "n/a",
-               val picture: String = "n/a",
-               val owner: String) {
-
-    @JsonIgnore
-    @PrimaryKey
-    val id: String = UUID.randomUUID().toString()
+data class Pet(
+        @JsonIgnore
+        @PrimaryKey
+        val id: String = UUID.randomUUID().toString(),
+        val name: String,
+        val type: String = "n/a",
+        val breed: String = "n/a",
+        val picture: String = "n/a",
+        val owner: String) {
 }
