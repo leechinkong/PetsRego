2c80f86bae6cd465f8af0b3d328bfb2cec86ca25
lee.c.kong@gmail.com <lee.kong@anz.com>
2018-08-07 01:35:01 +1000

    Fix controller

--- src/main/java/com/example/petsrego/controller/PetsRegoController.java
+++ src/main/java/com/example/petsrego/controller/PetsRegoController.java
@@ -15,14 +15,14 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-@Controller
+@RestController
 public class PetsRegoController {
 
   private final PetRepository petRepo;
